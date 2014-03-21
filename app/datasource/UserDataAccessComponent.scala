4696eec22fa81cfd403bc58eb97dc8edc7be4994
SimonKaltenbacher <simon.kaltenbacher@gmail.com>
2014-03-21 19:19:15 +0100

    Status quo

---
+++ app/datasource/UserDataAccessComponent.scala
@@ -0,0 +1,115 @@
+package app.datasource
+
+import scala.slick.driver.PostgresDriver.simple._
+import scala.slick.lifted.Shape
+
+/**
+  *
+  * @author Simon Kaltenbacher
+  */
+trait UserDataAccessComponent {
+
+  /** */
+  trait UserDataAccessModule {
+
+    /*
+     * Types
+     */
+
+    /** User data transfer object representation */
+    type User <: AbstractUser
+
+    /** User database table representation */
+    type UserTable <: AbstractUserTable
+
+    /*
+     * Shapes
+     */
+
+    /** */
+    implicit val userShape: Shape[_, UserTable, User, UserTable]
+
+    /*
+     * Data Transfer Objects
+     */
+
+    /** */
+    trait AbstractUser {
+
+      def id       : Int
+      def name     : String
+      def password : String
+    }
+
+    /*
+     * Database tables
+     */
+
+    /** */
+    trait AbstractUserTable extends Table[User] {
+
+      def id       : Column[Int]
+      def name     : Column[String]
+      def password : Column[String]
+    }
+
+    /*
+     * Queries
+     */
+
+    /** */
+    val users: TableQuery[UserTable]
+
+    /** */
+    def usersByName(name: String): Query[UserTable, User] = users.filter(_.name === name)
+  }
+}
+
+/**
+  *
+  * @author Simon Kaltenbacher
+  */
+trait UserDataAccessComponentImpl extends UserDataAccessComponent {
+
+  object userDataAccess extends UserDataAccessModule {
+
+    /*
+     * Types
+     */
+
+    type User = UserImpl
+
+    type UserTable = UserTableImpl
+
+    /*
+     * Shapes
+     */
+
+    implicit val userShape: Shape[_, UserTable, User, UserTable] = implicitly[Shape[_, UserTable, User, UserTable]]
+
+    /*
+     * Data transfer objects
+     */
+
+    protected case class UserImpl(id: Int, val name: String, val password: String) extends AbstractUser
+
+    /*
+     * Database tables
+     */
+
+    protected class UserTableImpl(tag: Tag) extends Table[User](tag, "app_user") with AbstractUserTable {
+
+      def id       = column[Int   ]("id", O.PrimaryKey, O.AutoInc)
+      def name     = column[String]("name")
+      def password = column[String]("password")
+
+      def *        = (id, name, password) <> (UserImpl.tupled, UserImpl.unapply)
+    }
+
+    /*
+     * Queries
+     */
+
+    val users = TableQuery[UserTable]
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "calendar"
+
+version := "0.1"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  "com.typesafe.akka" % "akka-actor_2.10" % "2.3.0",
+  "com.typesafe.akka" % "akka-slf4j_2.10" % "2.3.0",
+  "ch.qos.logback" % "logback-classic" % "1.1.1",
+  "com.typesafe.slick" % "slick_2.10" % "2.0.1"
+)     
+
+play.Project.playScalaSettings
