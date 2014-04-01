0b9b2e86d4dfc8503c816f695b0ea59f2782b4ca
Christoph Göttschkes <just.mychris@googlemail.com>
2014-04-01 21:55:10 +0200

    Added functionality to TagController

--- app/controllers/TagController.scala
+++ app/controllers/TagController.scala
@@ -1,21 +1,36 @@
 package controllers
 
+import akka.pattern.ask
+
 import access.Restricted
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json.Json.toJson
 
 import service._
 import service.protocol._
 
+import formatters.TagFormatter._
+
 object TagController extends Controller with Restricted with ExecutionEnvironment {
 
-  def show(id: Int) = Action {
-  	Status(501)("")
+  def show(id: Int) = Action.async {
+  	val req = (Services.calendarService ? GetTagById(id)).mapTo[Response]
+
+    req.map {
+      case TagById(tag)  => Ok(toJson(tag))
+      case DatabaseConnectionError(_)  => Ok("No connection to server!")
+    }
   }
 
-  def list() = Action {
-  	Status(501)("")
+  def list() = Action.async { implicit request =>
+  	val req = (Services.calendarService ? GetTagsFromUser(request.session.get("userid").map(_.toInt).get)).mapTo[Response]
+
+    req.map {
+      case TagsFromUser(tags)  => Ok(toJson(tags))
+      case DatabaseConnectionError(_)  => Ok("No connection to server!")
+    }
   }
 
   def add() = Action {
