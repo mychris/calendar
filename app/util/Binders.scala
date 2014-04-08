c347354d1eea7b36a2b19429c27043da998ad6c8
fliebhart <flo.liebhart@gmail.com>
2014-04-08 18:34:29 +0200

    Add implicit for String to DateTime for routes

--- app/util/Binders.scala
+++ app/util/Binders.scala
@@ -1,12 +1,14 @@
 package util
 
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
+import hirondelle.date4j.DateTime
 
 object Binders {
 
+  // TODO: How to pass date formatter in date4j?  (Needed is: "yyyy-MM-dd"!)
+  implicit def string2DateTime(date: String): DateTime = new DateTime(date)
+
   implicit def optionString2DateTime(dateOption: Option[String]): Option[DateTime] = dateOption match {
     case None       => None
-    case Some(date) => Some(DateTime.parse(date, DateTimeFormat.forPattern("yyyy-MM-dd")))
+    case Some(date) => Some(new DateTime(date))   // TODO: How to pass date formatter in date4j?  (Needed is: "yyyy-MM-dd"!)
   }
 }
