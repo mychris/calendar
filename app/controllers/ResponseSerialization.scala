663e181fa880041fc29ddb86571ee0e6a5578080
fliebhart <flo.liebhart@gmail.com>
2014-04-13 17:55:40 +0200

    Better error Message in frontend after Timeout and other Exceptions

--- app/controllers/ResponseSerialization.scala
+++ app/controllers/ResponseSerialization.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import akka.pattern.AskTimeoutException
+
 import formatters._
 
 import play.api.libs.json.{util => _, _}
@@ -12,11 +14,13 @@ import service._
 import service.protocol._
 
 import util.JsonConversion._
+import java.util.concurrent.TimeoutException
 
 /**
-  *
-  * @author Simon Kaltenbacher
-  */
+ *
+ * @author Simon Kaltenbacher
+ * @author Florian Liebhart
+ */
 trait ResponseSerialization {
 
   self: ExecutionEnvironment =>
@@ -30,5 +34,7 @@ trait ResponseSerialization {
         case NoSuchTagError(message)          => NotFound(message.toJson)
         case NoSuchAppointmentError(message)  => NotFound(message.toJson)
         case DatabaseError(message)           => InternalServerError(message.toJson)
+        case e: AskTimeoutException           => InternalServerError(e.getMessage)
+        case e: Exception                     => InternalServerError(e.getMessage)
       }
 }
