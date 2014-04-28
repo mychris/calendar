5e53b95f441d35eec2391d4909ee7ba0b0dabfca
SimonKaltenbacher <simon.kaltenbacher@gmail.com>
2014-04-28 16:34:36 +0200

    Fixed query string binding for user ids

--- app/controllers/Proposals.scala
+++ app/controllers/Proposals.scala
@@ -14,6 +14,7 @@ import scala.concurrent.{util => _, _}
 import hirondelle.date4j.DateTime
 
 import play.api.mvc._
+import play.api.Logger
 
 import service._
 import service.protocol._
--- app/util/Binders.scala
+++ app/util/Binders.scala
@@ -11,19 +11,6 @@ object Binders {
 
   val timeZone = TimeZone.getTimeZone("UTC")
 
-  implicit def bindableSeq[A](implicit bindableA: QueryStringBindable[A]) = new QueryStringBindable[Seq[A]] {
-
-    def bind(key: String, params: Map[String, Seq[String]]) = {
-
-      def bindSeq(values: Seq[String]): Seq[A] =
-        values.flatMap(a => bindableA.bind(a.trim, params)).flatMap(_.right.toOption)
-
-      Some(Right(bindSeq(key.split(",").toSeq)))
-    }
-
-    def unbind(key: String, value: Seq[A]) = value.map(bindableA.unbind(key, _)).mkString(",")
-  }
-
   implicit object bindableDateTime extends Parsing[DateTime](
     str => DateTime.forInstant(str.toLong, timeZone),
     _.getMilliseconds(timeZone).toString,
