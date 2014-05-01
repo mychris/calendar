b1ad5a51760430efbe3be2b434be0785e697b7f1
SimonKaltenbacher <simon.kaltenbacher@gmail.com>
2014-05-01 12:29:05 +0200

    Added joda datetime column type

--- app/util/CustomColumnTypes.scala
+++ app/util/CustomColumnTypes.scala
@@ -9,11 +9,9 @@ import scala.slick.driver.PostgresDriver.simple._
 
 object CustomColumnTypes {
 
-  val bla = dateColumnType
-  
-  implicit val dateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
-    dateTime  => new Timestamp(dateTime.getMilliseconds(TimeZone.getTimeZone("UTC")).toLong),
-    timestamp => DateTime.forInstant(timestamp.getTime.toLong, TimeZone.getTimeZone("UTC"))
+  implicit def dateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
+    dt => new Timestamp(dt.getMillis),
+    ts => new DateTime(ts.getTime)
   )
 
   implicit val colorColumnType = MappedColumnType.base[Color, String](
