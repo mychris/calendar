396a602e3a91e4bf88e670c964e2c7973f603753
fliebhart <flo.liebhart@gmail.com>
2014-05-03 21:50:36 +0200

    Always show default tag on top of the list

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -806,7 +806,16 @@ function listTags() {
 
       d3.select("#tags ul li.nonefound").remove();
 
-      var tags = d3.select("#tags ul").selectAll("li.tag").data(data.tags);
+      var defaultTag;
+      var tagsWithoutDefault = [];
+      for (var i = 0; i < data.tags.length; i++) {
+        if ( !(data.tags[i].id == defaultTagId) )
+          tagsWithoutDefault.push(data.tags[i]);
+        else
+          defaultTag = data.tags[i]
+      };
+
+      var tags = d3.select("#tags ul").selectAll("li.tag").data([defaultTag].concat(tagsWithoutDefault));
 
       /* Update */
       var filteredTagIds = $("#tags li.tag span.name.filter-off").parent().map(function() {
